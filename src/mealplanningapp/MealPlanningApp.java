ada30dd8acd8b88315b5f2ce3812b67f1ce34308
Alexander Alwardt <aalwardt@oakland.edu>
2018-09-26 11:07:44 -0400

    Commit example

--- src/mealplanningapp/MealPlanningApp.java
+++ src/mealplanningapp/MealPlanningApp.java
@@ -27,7 +27,7 @@ public class MealPlanningApp extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("TESTTTTTTTTT!");
             }
         });
         
