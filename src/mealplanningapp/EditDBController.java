876b10c4e1e76be8497e55eb9eaf1f4ee193a7d5
Alexander Alwardt <aalwardt@oakland.edu>
2018-10-12 21:25:53 -0400

    Fixes for fxml files

---
+++ src/mealplanningapp/EditDBController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mealplanningapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author alexa
+ */
+public class EditDBController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
