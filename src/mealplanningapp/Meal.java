8f1b8694b68c36f9855cac910bf875ab9970f66e
Alexander Alwardt <aalwardt@oakland.edu>
2018-10-16 10:40:31 -0400

    Add Protein and Carbs to Meal object

--- src/mealplanningapp/Meal.java
+++ src/mealplanningapp/Meal.java
@@ -18,19 +18,22 @@ public class Meal {
     private String name;
     private int calories;
     private Category category;
-    //To do: Protein? Carbs?
+    private int protein;
+    private int carbs;
 
     //Create a meal from an existing one (with an ID number)
-    public Meal(int id, String name, int calories, Category category){
+    public Meal(int id, String name, Category category, int calories, int protein, int carbs){
         this.id = id;
         this.name = name;
-        this.calories = calories;
         this.category = category;
+        this.calories = calories;
+        this.protein = protein;
+        this.carbs = carbs;
     }
     
     //Create a new meal without an ID number
-    public Meal(String name, int calories, Category category){
-        this(-1, name, calories, category); //Call full constructor with -1 as ID number
+    public Meal(String name, Category category, int calories, int protein, int carbs){
+        this(-1, name, category, calories, protein, carbs); //Call full constructor with -1 as ID number
     }
     
     //Getters
@@ -41,7 +44,7 @@ public class Meal {
     
     @Override
     public String toString() {
-        return String.format("%s, %s, %d calories", name, category.toString(), calories);
+        return String.format("%s, %s, %d calories, %dg Protein, %dg Carbs", name, category.toString(), calories, protein, carbs);
     }
     
 }
